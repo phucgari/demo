33815cb3650686e53dbf33824d6ca0e23996b83f
Doan van phuc <phucgari@gmail.com>
2016-12-22 11:36:05 -0800

    no message

--- hw-session7/hw-session7.py
+++ hw-session7/hw-session7.py
@@ -10,7 +10,7 @@ user_name = "admin"
 password = "admin"
 mongoengine.connect(db_name, host=host, port=port, username=user_name, password=password)
 
-class User(Document):
+class Game(Document):
     name= StringField()
     des= StringField()
     img= StringField()
@@ -21,34 +21,7 @@ def hello_world():
     return render_template("intro.html")
 @app.route("/game")
 def hobby():
-    game=[
-        {
-            "name":"League of Legend",
-            "des":"A very famous MOBA game - most played pc game right now with 100 million players a month",
-            "img":"http://www.legalsportsreport.com/wp-content/uploads/2015/02/LeagueofLegends-600x400.jpg",
-            "link":"https://lienminh.garena.vn/"
-        },
-        {
-            "name":"Hearthstone",
-            "des":"A A fast-paced strategy card game for everyone - more than 50 million players log in per month",
-            "img":"http://sohanews.sohacdn.com/thumb_w/600/2016/1-sau-co-vay-va-starcraft-den-luot-hearthstone-tro-thanh-muc-tieu-de-may-tinh-danh-bai-loai-nguoi-1459278008494-0-41-360-581-crop-1459278077832.jpg",
-            "link":"http://us.battle.net/hearthstone/en/"
-        },
-        {
-            "name":"Overwatch",
-            "des":" first-person shooter with unique, creative characters like superman",
-            "img":"http://image1.redbull.com/rbcom/010/2015-11-16/1331759970098_2/0010/1/600/400/2/overwatch-is-the-new-esports-shooter-game-from-blizzard.jpg",
-            "link":"https://playoverwatch.com/"
-        },
-        {
-            "name":"Dota2",
-            "des":"Another very good game with an impressive number of player - most played on steam ",
-            "img":"http://wlp.ninja/wallpaper/Dota-2-wallpaper-1920x1080-07-600x400.jpg",
-            "link":"http://blog.dota2.com/"
-        }
-
-    ]
-    return render_template("game.html",game_list=game)
+    return render_template("game.html",game_list=Game.objects)
 @app.route("/addgame", methods=["GET","POST"])
 def adding():
     if request.method=="GET":
@@ -61,6 +34,30 @@ def adding():
         game=User(name=namex, img=imgx ,des=desx,link=linkx)
         game.save()
         return "thank for your add"
+@app.route("/deletegame/<id>")
+def delete(id):
+    delete_obj=Game.objects().with_id(id)
+    if delete_obj is None:
+        return "wrong id"
+    else:
+        delete_obj.delete()
+        return "delete success"
+
+@app.route("/updategame/<id>",methods=["GET","POST"])
+def updating(id):
+    game = Game.objects().with_id(id)
+    if request.method=="GET":
+        return render_template("updating.html",object=game)
+    elif request.method=="POST":
+        if request.form["name"] is not "":
+            game.update(set__name=request.form["name"])
+        if request.form["des"] is not "":
+            game.update(set__des=request.form["des"])
+        if request.form["img"] is not "":
+            game.update(set__img=request.form["img"])
+        if request.form["link"] is not "":
+            game.update(set__link=request.form["link"])
+        return "updated succesful"
 
 if __name__ == '__main__':
     app.run()
